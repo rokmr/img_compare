b386421f7a5072ecda9b9922544c0613628d8f39
Rohit Kumar <sahil15rohit88@gmail.com>
2024-12-27 09:30:10 +0530

    Enhance image comparison functionality by allowing selection of images from two folders and adding a toggle for overlay slider. Improved image loading with support for multiple formats.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_image_comparison import image_comparison
 from streamlit_js_eval import streamlit_js_eval
 from PIL import Image
-
+import os
 # Custom theme settings
 st.set_page_config(
     page_title="Image-Comparison",
@@ -37,19 +37,28 @@ def main():
     for i, col in enumerate(cols):
         with col:  # Use 'with' statement to ensure everything is in the column
             folder_path = st.text_input("Folder path", value=f"folder{i+1}", key=f"folder_{i}")
-            image_path = f"{folder_path}/{i+1}.png"
-            image_comparison(
-                img1=image_path,
-                img2=image_path,
-                label1="Original",
-                label2="Modified",
-                width=width,
-                show_labels=True,
-                starting_position=50,
-                make_responsive=True,
-                in_memory=True
-            )
-
+            image_paths = [os.path.join(folder_path, f) for f in sorted(os.listdir(folder_path)) 
+                             if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+            use_image_comparison = st.toggle("overlay slider", value=True, key=f"use_image_comparison_{i}")
+            if use_image_comparison:
+                second_folder_path = st.text_input("Second folder path", value=f"folder{i+1}", key=f"second_folder_{i}")
+                second_image_paths = [os.path.join(second_folder_path, f) for f in sorted(os.listdir(second_folder_path)) 
+                                            if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+                for image_path1, image_path2 in zip(image_paths, second_image_paths):
+                    image_comparison(
+                        img1=image_path1,
+                        img2=image_path2,
+                        label1="Original",
+                        label2="Modified",
+                        width=width,
+                        show_labels=True,
+                        starting_position=50,
+                        make_responsive=True,
+                        in_memory=True
+                    )
+            else:
+                for image_path in image_paths:
+                    st.image(image_path, use_container_width=True)
 
 if __name__ == "__main__":
     main()
